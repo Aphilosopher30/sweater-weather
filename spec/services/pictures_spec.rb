a05b0f53c464b3c6ccdda0ccd4f1773cd20c2597
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-08-09 20:07:58 -0600

    create pictures service

---
+++ spec/services/pictures_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe PicturesService do
+
+  describe "returns picture " do
+    it "returns the desired pict" do
+
+      picture_api = File.read('spec/fixtures/unsplash/denver_co.json')
+
+
+      stub_request(:get, "https://api.unsplash.com/search/photos?client_id=-MKYh8bJYN9doJagJg6C9lrBleC3e0RFC3F9BECy8e8&query=denver,co").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.6.0'
+          }).
+        to_return(status: 200, body: picture_api, headers: {})
+
+
+        picture_response = PicturesService.city_picture('denver,co')
+        expected_response = JSON.parse(picture_api, symbolize_names: true)
+
+        expect(picture_response).to eq(expected_response)
+    end
+  end
+
+end
